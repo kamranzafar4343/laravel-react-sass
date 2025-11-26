e2d2991595c6f95731ecb2c171978a9f49217724
kamranzafar4343 <kamranzafar4343@gmail.com>
2025-11-27 00:25:14 +0500

    Add database seeder, models, and migrations for features, packages, transactions, and used features

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -18,8 +18,42 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'kamran',
+            'email' => 'kamran@mezan.com',
+            'password' => 'kamran@mezan'
+        ]);
+
+        Feature::create([
+            'image' => 'https://img.icons8.com/color/200/plus.png',
+            'name' => 'Calculate Sum',
+            'description' => 'calculate sum of two numbers',
+            'required_credits' => 1,
+            'active' => true,
+        ]);
+        Feature::create([
+            'image' => 'https://img.icons8.com/color/200/minus.png',
+            'name' => 'Calculate Difference',
+            'description' => 'calculate difference of two numbers',
+            'required_credits' => 3,
+            'active' => true,
+        ]);
+
+        Package::create([
+            'name' => 'Basic',
+            'price' => 5,
+            'credits' => 20,
+        ]);
+        
+        Package::create([
+            'name' => 'Silver',
+            'price' => 20,
+            'credits' => 100,
+        ]);
+        
+        Package::create([
+            'name' => 'Gold',
+            'price' => 100,
+            'credits' => 1000,
         ]);
     }
 }
