7c44f9f1cc557f37d513acb96e3741c15628b238
Kamran Zafar <91176244+kamranzafar4343@users.noreply.github.com>
2025-12-30 19:40:14 +0500

    refactor: update HomePageTest to use Pest's get function

--- tests/Feature/HomePageTest.php
+++ tests/Feature/HomePageTest.php
@@ -1,7 +1,11 @@
 <?php
 
+use function Pest\Laravel\get;
+
+/** @var \Illuminate\Testing\TestCase $this */
+
 test('example', function () {
-    $response = $this->get('/');
+    $response = get('/');
 
     $response->assertStatus(200);
 });
